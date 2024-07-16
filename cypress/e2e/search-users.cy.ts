9598d190ae5bf17eefca4a0462370b58309558f8
nesterone <igornester@gmail.com>
2024-07-16 17:55:53 +0200

    - check for users

--- cypress/e2e/search-users.cy.ts
+++ cypress/e2e/search-users.cy.ts
@@ -1,15 +1,12 @@
-// https://on.cypress.io/api
-
 describe('Search Users', () => {
   it('visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Github Users')
   })
 
-  it.skip('find users with default search results', () => {
+  it('has a users component', () => {
     cy.visit('/')
 
-    cy.contains('user')
-
+    cy.get('[data-testid=users]')
   })
 })
