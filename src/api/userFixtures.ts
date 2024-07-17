06ff7a13a56fe1a806befa830ef09655a1ce963c
nesterone <igornester@gmail.com>
2024-07-17 19:45:52 +0200

    - remove outdated

--- src/api/userFixtures.ts
+++ src/api/userFixtures.ts
@@ -1,23 +1,4 @@
-import type {User} from '@/api/User'
-
 export const userFixtures = {
-    defaultList(): User[] {
-        return [
-            {
-                avatar: 'https://avatars.githubusercontent.com/u/660378?v=4',
-                name: 'factset'
-            },
-            {
-                avatar: 'https://avatars.githubusercontent.com/u/132286975?v=4',
-                name: 'FactoryTalk-Optix'
-            },
-            {
-                avatar: 'https://avatars.githubusercontent.com/u/10263490?v=4',
-                name: 'fact'
-            }
-        ]
-    },
-
     successfulServerResponse() {
         return {
             total_count: 6082,
