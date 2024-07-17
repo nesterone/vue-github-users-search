ef0945cd1ca802238fafd7558472edcf9e00e163
nesterone <igornester@gmail.com>
2024-07-17 14:32:28 +0200

    - add test with fixture

--- src/components/UsersSearchResult.cy.ts
+++ src/components/UsersSearchResult.cy.ts
@@ -1,7 +1,23 @@
 import UsersSearchResult from './UsersSearchResult.vue'
+import {userFixtures} from "@/api/userFixtures";
 
 describe('<UsersSearchResult />', () => {
-  it('renders', () => {
+  it('request for search and renders the same amount of results', () => {
+
+    const expected = userFixtures.successfulServerResponse();
+    cy.intercept('https://api.github.com/search/users*',  expected).as('getSearch')
+
     cy.mount(UsersSearchResult, {props: {query: 'fact'}})
+
+    cy.get('[data-testid="message"]').should('have.text', 'User search returned no results.')
+    cy.get("[role='list']").should('not.exist')
+
+    cy.wait('@getSearch')
+
+    cy.get("[data-testid='users']")
+    cy.get("[role='list']")
+    cy.get("[role='listitem']").its('length').should('eq', expected.items.length)
+
+    cy.get('[data-testid="message"]').should('not.exist')
   })
 })
