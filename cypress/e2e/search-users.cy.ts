7f7ee6d1588434ca0187a14a9e4d917b2dd6cc98
nesterone <igornester@gmail.com>
2024-07-18 12:16:58 +0200

    - fix test

--- cypress/e2e/search-users.cy.ts
+++ cypress/e2e/search-users.cy.ts
@@ -1,23 +1,21 @@
+
+const MAX_ITEMS_DEFAULT = 25;
 describe('Search Users', () => {
   it('has a users component with items by default', () => {
     cy.visit('/')
 
-    cy.get('[data-testid=users] [role=listitem]').its('length').should('eq', 3);
+    cy.get('[data-testid=users] [role=listitem]').its('length').should('eq', MAX_ITEMS_DEFAULT);
   })
 
   it('types a new query and get results', () => {
     cy.visit('/')
 
     const query = 'test';
-    const queryRegexp = new RegExp(query, 'i')
 
     cy.get('input[name="search"]').clear().type(query).should('have.value', query);
 
     cy.contains('Loading...')
 
-    cy.get('[role=listitem]').filter((index, element) => {
-      cy.log(element.innerText)
-      return queryRegexp.test(element.innerText);
-    }).should('have.length', 3);
+    cy.get('[data-testid=users] [role=listitem]').its('length').should('eq', MAX_ITEMS_DEFAULT);
   });
 })
